b3df7b0a128846b7f61e48a9328d9360152509e3
Kim Hoalim <kthoalim@gmail.com>
2019-09-17 15:37:59 -0400

    InitialCommit

---
+++ MasteryProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MasteryProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
